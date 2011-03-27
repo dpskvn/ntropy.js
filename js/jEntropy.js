e7f755248e0fe1a6cb85a1afb182f68adc250659
Dino Paskvan <dpaskvan@gmail.com>
2011-03-28 00:48:54 +0200

    getting the code jQuery plugin standard compliant

--- js/jEntropy.js
+++ js/jEntropy.js
@@ -9,43 +9,57 @@
 ========================================================= */
 
 (function($) {
-  $.fn.jEntropy = function(options) {
-	/* set default options */
-    var options = $.extend({
-			target: '#output',
-			rSize: '10',
-			pool: 'abcdefghijklmnopqrstuvwxyz0123456789',
-			message: 'Keep moving the mouse.',
-			resetText: 'Reset'
-    }, options);
+  	var methods = {
+  		init : function(options) {
+  					var defaults = {
+  						target: '#output',
+						rSize: '10',
+						pool: 'abcdefghijklmnopqrstuvwxyz0123456789',
+						message: 'Keep moving the mouse.',
+						resetText: 'Reset'
+  					}
+  	
+    				var options = $.extend(defaults, options);
+    				var o = options;
 	
-	this.each(function() {
-			var base = $(this);
-			base.html('<div id="jEDetector"></div>');
-			$('#jEDetector').css('min-height', base.height());
-			$('#jEDetector').css('min-width', base.width());
-			$('#jEDetector').css('background', 'url(images/chaos.png) no-repeat ' + (base.height()/2-48) + 'px ' + (base.width()/2-48) + 'px');
-			var randomSeed = [];
-			var limit = 0;
-			var random = '';
-			$('#jEDetector').mousemove(function(e){
-				var tOffset = Math.floor((Number(new Date().getTime()))/Math.PI);
-				limit = randomSeed.push(tOffset - (e.pageX ^ e.pageY));
-				$(options.target).val(options.message);
-				if (limit == 250) {
-					$(this).hide();
-					base.html('<div id="jEDone"><input type="button" value="' + options.resetText + '" id="jEReset" /></div>');
-					$('#jEDone').css('min-height', base.height());
-					$('#jEDone').css('min-width', base.width());
-					$('#jEDone').css('background', 'url(images/done.png) no-repeat ' + (base.height()/2-48) + 'px ' + (base.width()/2-48) + 'px');
-					for (i = 0; i < options.rSize; i++) {
-						lottery = Math.floor(Math.random() * (limit + 1));
-						rindex = randomSeed[lottery] % options.pool.length;
-						random += options.pool.substring(rindex, rindex + 1);
-					}
-					$(options.target).val(random);
-				}
-			});
-    });
-  };
+					return this.each(function() {
+							var base = $(this);
+							base.html('<div id="jEDetector"></div>');
+							$('#jEDetector').css('min-height', base.height());
+							$('#jEDetector').css('min-width', base.width());
+							$('#jEDetector').css('background', 'url(images/chaos.png) no-repeat ' + (base.height()/2-48) + 'px ' + (base.width()/2-48) + 'px');
+							var randomSeed = [];
+							var limit = 0;
+							var random = '';
+							$('#jEDetector').mousemove(function(e){
+								var tOffset = Math.floor((Number(new Date().getTime()))/Math.PI);
+								limit = randomSeed.push(tOffset - (e.pageX ^ e.pageY));
+								$(o.target).val(o.message);
+								if (limit == 250) {
+									$(this).hide();
+									base.html('<div id="jEDone"><input type="button" value="' + o.resetText + '" id="jEReset" /></div>');
+									$('#jEDone').css('min-height', base.height());
+									$('#jEDone').css('min-width', base.width());
+									$('#jEDone').css('background', 'url(images/done.png) no-repeat ' + (base.height()/2-48) + 'px ' + (base.width()/2-48) + 'px');
+									for (i = 0; i < o.rSize; i++) {
+										lottery = Math.floor(Math.random() * (limit + 1));
+										rindex = randomSeed[lottery] % o.pool.length;
+										random += o.pool.substring(rindex, rindex + 1);
+									}
+									$(o.target).val(random);
+								}
+							});
+    				});
+  		}
+  	};
+  	
+  	$.fn.jEntropy = function (method) {
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.jEntropy');
+        }
+    };
 })(jQuery);
