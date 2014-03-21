228ff0f55c22a8b0f2d1a23dbd6a14b8345eac6f
Dino Paskvan <dpaskvan@gmail.com>
2014-03-21 22:55:32 +0100

    Introducing ntropy.js

--- README.md
+++ README.md
@@ -1,32 +1,30 @@
-#jEntropy - a jQuery plugin
+#ntropy.js
 
 ##Description
-jEntropy is a random string/number generator using entropy collected from mouse movement as the seed for random content.
+ntropy.js is a random string/number generator using entropy collected from mouse movement or device motion as the seed for random content.
 
 It can be used as way for users to generate passwords for themselves, generate password salts during account registration, or even manual session ID creation. There are other possible uses, generating encryption keys, and so on.
 
 ##Usage
 The usage is rather simple. Just point the plugin to the target input field. 
 
-		$(#inputID).jEntropy({
-		          'option1' : 'value1',
-		          'option2' : 'value2'
+		ntropy({limit: 10, pool: "abcdefghijklmnopqrstuvwxyz"}, function (random) {
+  		console.log(random);
 		});
+		
+After the random string is generated, the callback is executed.
+
+##Demo
+A demo/new page will follow soon.
 
 ##Options
-The options you can set are:
+The `options` object has the following two properties:
 
-rSize - the length of the generated string
+limit - the length of the generated string
 pool - the characters used to generate the string (i.e. 'abcdefghijklmopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-message - the message you want to display to your users while they are generating the string
-disable - boolean value which decides whether the input field should be disabled after the string has been generated
-
-The plugin has been tested with jQuery 1.4 and 1.5. It's working on latest versions of Chrome, Firefox, Safari, IE and Opera.
 
 ##License
-jEntropy is released under the MIT license. For more info, check the LICENSE file.
+ntropy.js is released under the MIT license. For more info, check the LICENSE file.
 
 ##Author
 Dino Paskvan (http://www.dinopaskvan.com)
-
-[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/DinoPaskvan/jentropy/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
---
+++ src/ntropy.js
@@ -0,0 +1,133 @@
+//=======================================================
+//              _
+//  _ __       | |_ _ __ ___  _ __  _   _   (_)___
+// | '_ \ _____| __| '__/ _ \| '_ \| | | |  | / __|
+// | | | |_____| |_| | | (_) | |_) | |_| |_ | \__ \
+// |_| |_|      \__|_|  \___/| .__/ \__, (_)/ |___/
+//                           |_|    |___/ |__/
+// n-tropy.js
+// Version: 2.0
+// Author: Dino Paskvan
+// Mail: dpaskvan@gmail.com
+// Web: http://www.dinopaskvan.com
+// Copyright (c) 2014 Dino Paskvan
+// Licence : MIT
+//=======================================================
+
+window.ntropy = (function () {
+  "use strict";
+  /**
+   * Calls ntropy to start the random generation process
+   * @param {Object} options - Set the pool and length for the random generator
+   * @param {Function} callback - Function to be exectuted after the number has been generated
+   */
+  return function (options, callback) {
+    var pool = options.pool || "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%",
+      limit = options.limit || 10,
+      seed = [],
+      oldMX = 0,
+      oldMY = 0,
+      oldX = 0,
+      oldY = 0,
+      oldZ = 0;
+
+    /**
+     * Compars if values have changed enough in relation to the supplied offset
+     * @param {Number} oldVal - Previous value
+     * @param {Number} newVal - New value
+     * @param {Number} offset - The offset to compare to
+     */
+
+    function cmpValues(oldVal, newVal, offset) {
+      if (oldVal > newVal) {
+        if ((oldVal - newVal) > offset) {
+          return true;
+        }
+        return false;
+      }
+      if ((newVal - oldVal) > offset) {
+        return true;
+      }
+      return false;
+    }
+
+    /**
+     * Checks for (compatible) mobile devices
+     */
+
+    function mobileCheck() {
+      if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        return true;
+      }
+      return false;
+    }
+
+    /**
+     * Uses the data from motion events to create seed for the random string
+     * @param {Object} e - The event
+     */
+
+    function handleMotionEvent(e) {
+      var x = e.accelerationIncludingGravity.x,
+        y = e.accelerationIncludingGravity.y,
+        z = e.accelerationIncludingGravity.z,
+        random = '',
+        tSeed,
+        i;
+      if (cmpValues(oldX, x, 2) || cmpValues(oldY, y, 2) || cmpValues(oldZ, z, 2)) {
+        tSeed = Math.abs((Math.floor(x * 100) ^ Math.floor(y * 100)) ^ Math.floor(z * 100)) % (pool.length * 3);
+        if (tSeed < pool.length) {
+          seed.push(tSeed);
+        }
+        if (seed.length === limit) {
+          for (i = 0; i < seed.length; i = i + 1) {
+            random += pool[seed[i]];
+            if (random.length === limit) {
+              window.removeEventListener('devicemotion', handleMotionEvent, true);
+              callback(random);
+            }
+          }
+        }
+        oldX = x;
+        oldY = y;
+        oldZ = z;
+      }
+    }
+
+    /**
+     * Uses the data from mouse movement events to create seed for the random string
+     * @param {Object} e - The event
+     */
+
+    function handleMouseEvent(e) {
+      var x = e.clientX,
+        y = e.clientY,
+        random = '',
+        tSeed,
+        i;
+      if (cmpValues(oldMX, x, 51) || cmpValues(oldMY, y, 51)) {
+        tSeed = (x ^ y) % (pool.length * 3);
+        if (tSeed < pool.length) {
+          seed.push(tSeed);
+        }
+        if (seed.length === limit) {
+          for (i = 0; i < seed.length; i = i + 1) {
+            random += pool[seed[i]];
+            if (random.length === limit) {
+              window.removeEventListener('mousemove', handleMouseEvent, true);
+              callback(random);
+            }
+          }
+        }
+        oldMX = x;
+        oldMY = y;
+      }
+    }
+
+    if (window.DeviceOrientationEvent && mobileCheck()) {
+      window.addEventListener('devicemotion', handleMotionEvent, true);
+    } else {
+      window.addEventListener('mousemove', handleMouseEvent, true);
+    }
+  };
+}());
